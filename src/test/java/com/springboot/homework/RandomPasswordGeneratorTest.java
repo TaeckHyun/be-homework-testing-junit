ae95e66b5303128934303b8ba78e6e705eed285f
택현 <tjsk2222@gmail.com>
2025-01-21 15:32:10 +0900

    JUnit 실습

--- src/test/java/com/springboot/homework/RandomPasswordGeneratorTest.java
+++ src/test/java/com/springboot/homework/RandomPasswordGeneratorTest.java
@@ -1,12 +1,49 @@
 package com.springboot.homework;
 
+import com.springboot.helper.RandomPasswordGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class RandomPasswordGeneratorTest {
     @DisplayName("실습 3: 랜덤 패스워드 생성 테스트")
     @Test
     public void generateTest() {
         // TODO 여기에 테스트 케이스를 작성해주세요.
+
+        // given
+        int numberOfUpperCaseLetters = 2;
+        int numberOfLowerCaseLetters = 5;
+        int numberOfNumeric = 2;
+        int numberOfSpecialChars = 1;
+
+
+        // when
+        String randomPassWord = RandomPasswordGenerator.generate(numberOfUpperCaseLetters
+                ,numberOfLowerCaseLetters,numberOfNumeric,numberOfSpecialChars);
+        long totalLength = randomPassWord.length();
+
+        long upperCaseCount = randomPassWord.chars().filter(ch -> Character.isUpperCase(ch)).count();
+
+        long lowerCaseCount = randomPassWord.chars().filter(ch -> Character.isLowerCase(ch)).count();
+
+        long numericCount = randomPassWord.chars().filter(ch -> Character.isDigit(ch)).count();
+
+        long specialCharsCount = randomPassWord.chars().filter(ch -> !Character.isLetterOrDigit(ch)).count();
+
+        // then
+        assertNotNull(randomPassWord, "비번은 Null이면 안됨");
+
+        assertEquals(totalLength, randomPassWord.length(), "패스워드 길이가 예상이랑 다른뎀");
+
+        assertEquals(numberOfUpperCaseLetters, upperCaseCount, "대문자 개수가 일치하지 않습니다.");
+
+        assertEquals(numberOfLowerCaseLetters, lowerCaseCount, "소문자 개수가 일치하지 않습니다.");
+
+        assertEquals(numberOfNumeric, numericCount, "0이상의 숫자 개수가 일치하지 않습니다.");
+
+        assertEquals(numberOfSpecialChars, specialCharsCount, "특수문자 개수가 일치하지 않습니다.");
     }
 }
--- src/test/java/com/springboot/homework/StampCalculatorTest.java
+++ src/test/java/com/springboot/homework/StampCalculatorTest.java
@@ -1,18 +1,49 @@
 package com.springboot.homework;
 
+import com.springboot.helper.StampCalculator;
+import com.springboot.order.entity.Order;
+import com.springboot.order.entity.OrderCoffee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StampCalculatorTest {
     @Test
     @DisplayName("실습1: 스탬프 카운트 계산 단위 테스트")
     public void calculateStampCountTest() {
         // TODO 여기에 테스트 케이스를 작성해주세요.
+        int nowCount = 5;
+        int earned = 3;
+
+        int actual = StampCalculator.calculateStampCount(nowCount, earned);
+
+        int expected = 8;
+        assertEquals(expected, actual);
     }
 
     @Test
     @DisplayName("실습1: 주문 후 누적 스탬프 카운트 계산 단위 테스트")
     public void calculateEarnedStampCountTest(){
         // TODO 여기에 테스트 케이스를 작성해주세요.
+        // given
+        Order order = new Order();
+        OrderCoffee orderCoffee1 = new OrderCoffee();
+        OrderCoffee orderCoffee2 = new OrderCoffee();
+
+        orderCoffee1.setQuantity(3);
+        orderCoffee2.setQuantity(5);
+
+        // when
+        order.setOrderCoffees(List.of(orderCoffee1, orderCoffee2));
+
+        int expected = orderCoffee1.getQuantity() + orderCoffee2.getQuantity();
+
+        int actual = StampCalculator.calculateEarnedStampCount(order);
+
+        // then
+        assertEquals(expected, actual);
     }
 }
